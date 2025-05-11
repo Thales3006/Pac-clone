10dffa007af8d58f2971baf5f93e127eed06bbd1
thales3006 <thales3006@gmail.com>
2025-05-11 02:26:13 -0300

    sprites added

--- internal/game/scene-level.go
+++ internal/game/scene-level.go
@@ -1,6 +1,7 @@
 package game
 
 import (
+	"math"
 	"pac-clone/internal/entities"
 	"pac-clone/internal/level"
 	mv "pac-clone/internal/movement"
@@ -13,12 +14,37 @@ import (
 )
 
 var (
-	level_loaded bool = false
-	blinkyTex    rl.Texture2D
-	pinkyTex     rl.Texture2D
-	inkyTex      rl.Texture2D
-	clideTex     rl.Texture2D
-	start        *utils.Timer
+	level_loaded    bool = false
+	blinkyTex       rl.Texture2D
+	pinkyTex        rl.Texture2D
+	inkyTex         rl.Texture2D
+	clideTex        rl.Texture2D
+	tile            rl.Texture2D
+	start           *utils.Timer
+	tileTableStreet = map[Conection]rl.Vector2{
+		Horizontal: {X: 2, Y: 1},
+		Vertical:   {X: 3, Y: 1},
+		None:       {X: 4, Y: 1},
+	}
+	tileTable = map[level.Cell][]rl.Vector2{
+		level.Wall:  {{X: 0, Y: 0}, {X: 1, Y: 0}, {X: 2, Y: 0}, {X: 3, Y: 0}, {X: 4, Y: 0}, {X: 5, Y: 0}, {X: 6, Y: 0}, {X: 7, Y: 0}, {X: 8, Y: 0}, {X: 9, Y: 0}, {X: 0, Y: 1}, {X: 1, Y: 1}},
+		level.Point: {{X: 0, Y: 2}, {X: 1, Y: 2}, {X: 2, Y: 2}, {X: 9, Y: 1}},
+		level.Power: {{X: 3, Y: 2}},
+	}
+	degree = map[entities.Direction]float32{
+		entities.Right: 0,
+		entities.Left:  180,
+		entities.Up:    270,
+		entities.Down:  90,
+	}
+)
+
+type Conection int32
+
+const (
+	None       Conection = 0
+	Horizontal Conection = iota
+	Vertical
 )
 
 func (g *Game) HandleLevel() {
@@ -103,10 +129,12 @@ func (g *Game) loadLevel() {
 	pinkyTex = rl.LoadTexture("assets/pinky.png")
 	inkyTex = rl.LoadTexture("assets/inky.png")
 	clideTex = rl.LoadTexture("assets/clyde.png")
+	tile = rl.LoadTexture("assets/tilemap.png")
 
 	g.ResetPositions()
 	g.Player.Direction = entities.None
 	g.Player.Health = 5
+	g.Player.Score = 0
 	g.Player.IsDead = false
 
 	level_loaded = true
@@ -144,18 +172,25 @@ func (g *Game) Draw(bounds rl.Rectangle, drawEntities bool) {
 
 			cellRect.X = bounds.X + float32(j)*cellRect.Width
 			cellRect.Y = bounds.Y + float32(i)*cellRect.Height
-			switch cell {
-			case level.Wall:
-				rl.DrawRectangleRec(cellRect, rl.Blue)
-			case level.Door:
+			switch {
+			case cell == level.Wall:
+				DrawTile(tile, tileTable[level.Wall][mv.Mod(int32(i)+int32(j), int32(len(tileTable[level.Wall])))], cellRect)
+			case cell == level.Door:
 				rl.DrawRectangleRec(cellRect, rl.Gray)
+			default:
+				DrawTile(tile, tileTableStreet[getConection(g.Level, int32(i), int32(j))], cellRect)
+
+			}
+
+			switch cell {
 			case level.Point:
-				rl.DrawRectangle(
-					int32(cellRect.X+0.3*cellRect.Width),
-					int32(cellRect.Y+0.3*cellRect.Height),
-					int32(0.3*cellRect.Width),
-					int32(0.3*cellRect.Height),
-					rl.White)
+				DrawTile(tile, tileTable[level.Point][mv.Mod(int32(i)+int32(j), int32(len(tileTable[level.Point])))], rl.Rectangle{
+					X:      cellRect.X + (0.25+float32(math.Cos(rl.GetTime())/20))*cellRect.Width,
+					Y:      cellRect.Y + (0.25+float32(math.Sin(rl.GetTime())/20))*cellRect.Height,
+					Width:  0.5 * cellRect.Width,
+					Height: 0.5 * cellRect.Height,
+				})
+
 			case level.Power:
 				rl.DrawRectangle(
 					int32(cellRect.X+0.2*cellRect.Width),
@@ -164,20 +199,19 @@ func (g *Game) Draw(bounds rl.Rectangle, drawEntities bool) {
 					int32(0.5*cellRect.Height),
 					rl.Orange)
 			}
+
 		}
 	}
 
 	if !drawEntities {
 		return
 	}
-
-	rl.DrawRectangleRec(rl.Rectangle{
-		X:      bounds.X + cellRect.Width*(g.Player.X+(1-g.Player.Width)/2),
-		Y:      bounds.Y + cellRect.Height*(g.Player.Y+(1-g.Player.Height)/2),
-		Width:  cellRect.Width * g.Player.Width,
-		Height: cellRect.Height * g.Player.Height,
-	},
-		rl.Yellow)
+	DrawTileRot(tile, rl.Vector2{X: 3, Y: 2}, rl.Rectangle{
+		X:      bounds.X + cellRect.Width*(g.Player.X+0.5),
+		Y:      bounds.Y + cellRect.Height*(g.Player.Y+0.5),
+		Width:  cellRect.Width,
+		Height: cellRect.Height,
+	}, degree[g.Player.Direction])
 
 	for _, ghost := range g.Ghosts {
 
@@ -215,14 +249,13 @@ func (g *Game) Draw(bounds rl.Rectangle, drawEntities bool) {
 			texture = &clideTex
 		}
 
-		texture.Width = int32(cellRect.Width * ghost.Width)
-		texture.Height = int32(cellRect.Height * ghost.Height)
-
-		srcRect := rl.NewRectangle(0, 0, float32(texture.Width), float32(texture.Height))
+		srcRect := rl.NewRectangle(0, 0, float32(cellRect.Width*ghost.Width), float32(cellRect.Height*ghost.Height))
 		position := rl.NewVector2(
 			bounds.X+cellRect.Width*(ghost.X+(1-ghost.Width)/2),
 			bounds.Y+cellRect.Height*(ghost.Y+(1-ghost.Height)/2),
 		)
+		texture.Width = int32(cellRect.Width * ghost.Width)
+		texture.Height = int32(cellRect.Height * ghost.Height)
 		rl.DrawTextureRec(*texture, srcRect, position, rl.White)
 	}
 }
@@ -234,5 +267,52 @@ func (g *Game) ResetPositions() {
 	for _, ghost := range g.Ghosts {
 		ghost.X = float32(g.Level.SpawnGhost[1])
 		ghost.Y = float32(g.Level.SpawnGhost[0])
+		ghost.State = entities.Chase
 	}
+}
+
+func DrawTile(tileTexture rl.Texture2D, pos rl.Vector2, bounds rl.Rectangle) {
+	const tileSize = 32
+
+	source := rl.NewRectangle(
+		float32(pos.X*tileSize),
+		float32(pos.Y*tileSize),
+		tileSize, tileSize,
+	)
+	rl.DrawTexturePro(tileTexture, source, bounds, rl.NewVector2(0, 0), 0, rl.White)
+}
+
+func DrawTileRot(tileTexture rl.Texture2D, pos rl.Vector2, bounds rl.Rectangle, degree float32) {
+	const tileSize = 32
+
+	source := rl.NewRectangle(
+		float32(pos.X*tileSize),
+		float32(pos.Y*tileSize),
+		tileSize, tileSize,
+	)
+	rl.DrawTexturePro(tileTexture, source, bounds, rl.Vector2{X: bounds.Width * 0.5, Y: bounds.Height * 0.5}, degree, rl.White)
+}
+
+func getConection(l *level.Level, i, j int32) Conection {
+	conection := None
+	if c := l.Grid[mv.Mod(i-1, l.Height)][j]; c != level.Wall && c != level.Door {
+		conection = Vertical
+	} else if c := l.Grid[mv.Mod(i+1, l.Height)][j]; c != level.Wall && c != level.Door {
+		conection = Vertical
+	}
+
+	if c := l.Grid[i][mv.Mod(j-1, l.Width)]; c != level.Wall && c != level.Door {
+		if conection == Vertical {
+			conection = None
+		} else {
+			conection = Horizontal
+		}
+	} else if c := l.Grid[i][mv.Mod(j+1, l.Width)]; c != level.Wall && c != level.Door {
+		if conection == Vertical {
+			conection = None
+		} else {
+			conection = Horizontal
+		}
+	}
+	return conection
 }
